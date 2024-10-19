3696345c7ac256fce9067004a311c39a5fff9c4a
Dylan Compton <dylancomptontx@gmail.com>
2024-10-19 17:36:29 -0500

    this feels like a good start

---
+++ lecture1/lectureOne.js
@@ -0,0 +1,8 @@
+/* Peak finder:
+* Given the array [a, b, c, d, e, f, g, h, i],
+* Position 2 is a peak iff b >= a && b >= c. 
+* Position 9 is a peak iff i >= h.
+* Find a peak if it exists. <--see 19:36 notes
+*/
+
+//Straightforward algo
